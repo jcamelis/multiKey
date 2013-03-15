53a3dea508b7d93d7f07e20973866c556232351f
jcamelis <jcamelis@jcamelis-ThinkPad.(none)>
2013-03-15 18:05:28 -0300

    Methods each and getKeys added.

--- jcamelis.multiKey.js
+++ jcamelis.multiKey.js
@@ -4,34 +4,56 @@
 * @website http://jquerylab.com.ar	
 */
 (function(){
-function MultiKey() {
-	this.keys = {};
-	this.value = null;
-}
+	function MultiKey() {
+		this.keys = {};
+		this.value = null;
+	}
+	/**
+	* @public
+	* @param {array}
+	* @param {value} Object
+	*/
+	MultiKey.prototype.set = function(args, value) {
+		var key = args.shift();
+		if (!this.keys[key]) {
+			this.keys[key] = new MultiKey(1);
+		}
+		if (args.length) {
+			this.keys[key].set(args, value);	
+		} else {
+			this.keys[key].value = value;
+		}
+	}
 
-MultiKey.prototype.set = function(args, value) {
-	var key = args.shift();
-	if (!this.keys[key]) {
-		this.keys[key] = new MultiKey(1);
+	MultiKey.prototype.get = function() {
+		if (!arguments.length) return null;
+		var args = [];
+		for (var i = 0; i < arguments.length; i++) {
+			args.push(arguments[i]);
+		}
+		var key = args.shift();
+		if (key && this.keys[key]) {
+			return this.keys[key].get(args);
+		}
+		return this.value;
 	}
-	if (args.length) {
-		this.keys[key].set(args, value);	
-	} else {
-		this.keys[key].value = value;
-	}
-}
 
-MultiKey.prototype.get = function() {
-	if (!arguments.length) return null;
-	var args = [];
-	for (var i = 0; i < arguments.length; i++) {
-		args.push(arguments[i]);
+	MultiKey.prototype.each = function(callback) {
+		if (typeof callback !== 'function') return;
+		var i = 0;
+		for (var key in this.keys) {
+			callback.apply(this.keys[key],[i, key]);
+			i++;
+		}
 	}
-	var key = args.shift();
-	if (key && this.keys[key]) {
-		return this.keys[key].get(args);
+
+	MultiKey.prototype.getKeys = function(callback) {
+		var tmp = [];
+		for (var key in this.keys) {
+			tmp.push(key)
+		}
+		return tmp;
 	}
-	return this.value;
-}
+
 window.MultiKey = MultiKey;
 }());
