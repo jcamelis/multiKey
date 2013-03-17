a4e1ed3bcc74e9a3a02af93ccc9449013d5a13f0
javier <javier@javier-K55A.(none)>
2013-03-17 20:40:00 -0300

    Typo on minify version.

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
-java -jar scripts/compiler.jar --js jcamelis.multiKey.js --js_output_file jcamelis.multiKey.js.min.js
+java -jar scripts/compiler.jar --js jcamelis.multiKey.js --js_output_file jcamelis.multiKey.min.js
 
 cat license.txt >> temp.txt
-cat jcamelis.multiKey.js.min.js >> temp.txt
-cat temp.txt > jcamelis.multiKey.js.min.js
+cat jcamelis.multiKey.min.js >> temp.txt
+cat temp.txt > jcamelis.multiKey.min.js
 
 rm temp.txt
